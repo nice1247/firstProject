74096d63eee522bbb413c10189cc3c6b87cea1e5
admin <admin@YD02-20>
2021-09-14 11:58:45 +0900

    9/14 12:00

---
+++ firstProject/src/firstProject/VariableExample.java
@@ -0,0 +1,28 @@
+package firstProject;
+
+public class VariableExample {
+	public static void main(String[] args) {
+		int number1 = 10; // 변수 <--> 상수
+		System.out.println("결과 : " + number1);
+		number1 = 20;
+		System.out.println("결과 : " + number1);
+		number1 = 30;
+		System.out.println("결과 : " + number1);
+		
+//		int number1 = 90; 동일 변수선언해서 에러
+		int number3 = 90;
+		int result;
+//		int-정수, double-실수
+		
+		final int number2 = 20;
+//		number2 = 30; 이건 상수이기 때문에 에러
+		
+		result = number1 + number2;
+		double result1 = number3 / 11;
+		System.out.println("결과값 : " + result1);
+		System.out.println("결과값 : " + result);
+		System.out.println("결과 : " + number1 + number2);
+		
+	}
+
+}
